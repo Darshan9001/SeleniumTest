d0dfd605cedd931f4391043c8fb76d5757d32dbc
Administrator <Administrator@172.31.0.137>
2024-03-10 12:25:14 +0000

    Adding all my files

---
+++ seleniumTest/src/locator/RelativeLocatorExample.java
@@ -0,0 +1,22 @@
+package locator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.apache.*;
+
+
+public class RelativeLocatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("file:///C:/Users/Administrator/Documents/demo.html");
+
+	}
+
+}
